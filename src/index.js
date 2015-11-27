8282e988b1c1c48f3513c21650cfb59077b7ba40
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2015-11-27 09:19:11 -0800

    add index.js file

---
+++ src/index.js
@@ -0,0 +1,7 @@
+var Grid = require('./ui/grid.js');
+var NoContributionsGrid = require('./ui/no-contributions-grid.js');
+
+module.exports = {
+  ContributionsGrid: Grid,
+  NoContributionsGrid: NoContributionsGrid
+};
